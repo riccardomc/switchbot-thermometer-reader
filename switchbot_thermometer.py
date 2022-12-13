c6e4e54b1cffe9bf72597589e1607d62242f73b7
riccardomc <riccardo.cefala@gmail.com>
2022-12-13 22:09:33 +0100

    get rid of mqtt

--- switchbot_thermometer.py
+++ switchbot_thermometer.py
@@ -2,10 +2,6 @@ import asyncio
 import json
 
 from bleak import BleakScanner
-import paho.mqtt.client as mqtt
-
-import traceback
-from datetime import datetime
 
 import logging
 
@@ -16,19 +12,6 @@ addresses = set([])
 service_data_id = "00000d00-0000-1000-8000-00805f9b34fb"
 
 devices_data = {}
-
-# MQTT
-mqtt_client = None
-mqtt_username = ""
-mqtt_password = ""
-mqtt_hostname = ""
-mqtt_hostport = 1883
-mqtt_timeout = 30
-
-# FIXME: MQTT implementation taken from:
-# https://github.com/bbostock/Switchbot_Py_Meter/blob/master/meters.py
-MQTT_TOPIC_STACK = {""}
-MQTT_PAYLOAD_STACK = {""}
 
 
 def decode(data):
@@ -72,102 +55,30 @@ def detection_callback(device, data):
     service_data = data.service_data.get(service_data_id)
     if service_data:
         if device.address not in addresses:
-            logger.info("found device: %s" % device.address)
+            logger.info("found new device: %s" % device.address)
             addresses.add(device.address)
-            discover(device.address)
         decoded_service_data = decode(service_data)
-        publish(device.address, decoded_service_data)
         devices_data[device.address] = decoded_service_data
 
 
-def mqtt_connect():
-    global mqtt_client
-    global mqtt_connected
-    mqtt_client = mqtt.Client()
-    mqtt_client.username_pw_set(mqtt_username, mqtt_password)
-
-    def _on_connect(client, _, flags, return_code):
-        global mqtt_connected
-        mqtt_connected = True
-        logger.info("MQTT connected: %s" % mqtt.connack_string(return_code))
-
-    def _on_disconnect(client, userdata, return_code):
-        if return_code != 0:
-            logger.critical("MQTT Client disconnected with code: %d", return_code)
-
-    def _on_publish(client, userdata, mid):
-        pass
-
-    def _on_log(mqttc, obj, level, string):
-        logger.info("%s %s" % (level, string))
-
-    mqtt_client.on_connect = _on_connect
-    mqtt_client.on_publish = _on_publish
-    mqtt_client.on_log = _on_log
-
-    mqtt_client.connect(mqtt_hostname, mqtt_hostport, mqtt_timeout)
-    mqtt_client.loop_start()
-
-
-def discover(address):
-    """
-    https://www.home-assistant.io/docs/mqtt/discovery/
-    """
-
-    address_formatted = address.replace(":", "")
-    # discovery_prefix>/<component>/[<node_id>/]<object_id>/config
-    topic_temperature = "homeassistant/sensor/%s_T/config" % address_formatted
-
-    message = {
-        "device_class": "temperature",
-        "name": "%s_T" % address_formatted,
-        "state_topic": "homeassistant/sensor/%s_T/state" % address_formatted,
-        "unit_of_measurement": "°C",
-        "unique_id": "%s_T" % address_formatted,
-        "value_template": "{{ value_json.raw_values.temperature_value }}",
-    }
-
-    mqtt_client.publish(topic_temperature, json.dumps(message), qos=0, retain=True)
-
-
-def publish(address, data):
-    try:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        data["timestamp"] = timestamp
-        message = json.dumps(data)
-        address_formatted = address.replace(":", "")
-        topic = "homeassistant/sensor/%s_T/state" % address_formatted
-        MQTT_TOPIC_STACK.add(topic)
-        MQTT_PAYLOAD_STACK.add(message)
-        if mqtt_client:
-            while len(MQTT_TOPIC_STACK) > 0:
-                t = MQTT_TOPIC_STACK.pop()
-                p = MQTT_PAYLOAD_STACK.pop()
-                if len(t) > 0:
-                    mqtt_client.publish(t, p, qos=0, retain=True)
-        else:
-            logger.warn("MQTT publish: can't send, no client")
-    except Exception:
-        logger.warn("MQTT publish: exception while sending")
-        traceback.print_exc()
-
-
-async def scan():
-    mqtt_connect()
+async def scan(time=10, sleep_interval=0.10):
     scanner = BleakScanner()
     scanner.register_detection_callback(detection_callback)
     await scanner.start()
-    while True:
-        await asyncio.sleep(0.10)
+    count_down = time
+    while count_down > 0:
+        await asyncio.sleep(sleep_interval)
+        count_down -= sleep_interval
     await scanner.stop()
 
 
 async def main():
-    try:
-        await asyncio.wait_for(scan(), timeout=1200.0)
-        print(json.dumps(devices_data, indent=2))
-        await asyncio.sleep(5)
-    except asyncio.TimeoutError:
-        print("got no devices")
+    while True:
+        try:
+            await asyncio.wait_for(scan(), timeout=10)
+            print(json.dumps(devices_data, indent=2))
+            await asyncio.sleep(5)
+        except asyncio.TimeoutError:
+            print("got no devices")
 
 asyncio.run(main())
